557b14f596d7c40435ad299ad206af1e128fffba
mbalabanov <marincomics@gmail.com>
2022-07-11 15:00:11 +0300

    Adds simple procedural solution for FizzBuzz using JS

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,17 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
+for (let number = 1; number <= 15; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    answer.push('FizzBuzz')
+  } else if (number % 3 === 0) {
+    answer.push('Fizz')
+  } else if (number % 5 === 0) {
+    answer.push('Buzz')
+  } else {
+    answer.push(number)
+  }
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
